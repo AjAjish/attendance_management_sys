73a3f07f9f645569e5d8ccee46f68866d499a546
unknown <abiajish@gmail.com>
2025-08-30 15:19:48 +0500

    Adding Pages

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,12 +3,18 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home/<uuid:id>/', views.home, name='home_with_id'),
     path('login/', views.login, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/<uuid:id>/', views.dashboard, name='dashboard_with_id'),
     path('student/', views.student, name='student'),
+    path('student/<uuid:id>/', views.student, name='student_with_id'),
     path('manage/', views.manage, name='manage'),
+    path('manage/<uuid:id>/', views.manage, name='manage_with_id'),
+    path('logout/', views.logout, name='logout'),
 ]
 
 if settings.DEBUG:
--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib import messages
+from .models import User
 
-def home(request):
+def base(request):
     return render(request, 'base/base.html')
 
+def home(request, id=None):
+    if id:
+        user = User.objects.get(id=id)
+        return render(request, 'home.html', {'id': id})
+    return render(request, 'home.html')
+
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(f"Username: {username}, Password: {password}")  # Debugging line
+        try:
+            # To check existing user
+            user = User.objects.get(username=username)
+            if user.password == password:  
+                id = str(user.id)
+                # Make it userid on session for after login
+                request.session['id'] = id
+                messages.success(request, 'Login successful!')
+                return redirect('dashboard_with_id', id=id)
+            # Check if user is admin
+            elif user.role == "admin" and user.password == password:
+                id = str(user.id)
+                request.session['id'] = id
+                messages.success(request, 'Login successful!')
+                return redirect('dashboard_with_id', id=id)
+            else:
+                return render(request, 'login.html', {'error': 'Invalid username or password'})
+        except User.DoesNotExist:
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
+
     return render(request, 'login.html')
 
-def dashboard(request):
+def dashboard(request, id=None):
+    if id:
+        user = User.objects.get(id=id)
+        return render(request, 'dashboard.html', {'id': id})
     return render(request, 'dashboard.html')
 
-def student(request):
+def student(request, id=None):
+    if id:
+        user = User.objects.get(id=id)
+        return render(request, 'student.html', {'id': id})
     return render(request, 'student.html')
 
-def manage(request):
+def manage(request, id=None):
+    if id:
+        user = User.objects.get(id=id)
+        return render(request, 'manage.html', {'id': id})
     return render(request, 'manage.html')
+
+def logout(request):
+    # Clear the session data
+    request.session.flush()
+    messages.success(request, 'Logout successful!')
+    return redirect('home')
